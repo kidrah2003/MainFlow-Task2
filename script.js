68d0821ea83b70c93bcde87d66e000708f0f4cba
kidrah2003 <kidrah2003@gmail.com>
2024-11-02 14:09:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Slideshow
+let currentSlide = 0;
+const slides = ["animal1.jpg", "animal2.jpg", "animal3.jpg", "animal4.jpg", "animal5.jpg"];
+
+function showSlide(index) {
+    document.getElementById('slide').src = slides[index];
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+// Initial slide
+showSlide(currentSlide);
